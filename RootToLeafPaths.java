f42378607d4f4f4df245f461b1015840b3d1aa77
chinmay768 <chinmaysahani07@gmail.com>
2024-09-23 23:50:19 +0530

    Root to leaf paths

--- RootToLeafPaths.java
+++ RootToLeafPaths.java
@@ -15,14 +15,37 @@ public class RootToLeafPaths {
              paths.add(new ArrayList<>(currPath));
          }
 
-         rootToLeafPaths(root.left, paths, new ArrayList<>(currPath));
-         rootToLeafPaths(root.right, paths, new ArrayList<>(currPath));
+        rootToLeafPathsPassByValue(root.left, paths, new ArrayList<>(currPath));
+        rootToLeafPathsPassByValue(root.right, paths, new ArrayList<>(currPath));
+    }
+
+    public static void rootToLeafPathsPassByRef(Node root, List<List<Integer>> paths, List<Integer> currPath){
+        if(Objects.isNull(root)){
+            return;
+        }
+
+        currPath.add(root.data);
+
+        if(root.left == null && root.right == null){
+            paths.add(new ArrayList<>(currPath));
+        }
+
+        rootToLeafPathsPassByRef(root.left, paths, currPath);
+        rootToLeafPathsPassByRef(root.right, paths, currPath);
+        currPath.removeLast();
+    }
+
+    public static void findRootToLeafPaths2(Node root){
+        List<List<Integer>> paths = new ArrayList<>();
+        List<Integer> currPath = new ArrayList<>();
+        rootToLeafPathsPassByRef(root, paths, currPath); //Single curr path
+        System.out.println(paths);
     }
 
     public static void findRootToLeafPaths(Node root) {
         List<List<Integer>> paths = new ArrayList<>();
         List<Integer> currPath = new ArrayList<>();
-        rootToLeafPathsPassByValue(root, paths, currPath);
+        rootToLeafPathsPassByValue(root, paths, currPath); //Multiple curr paths
         System.out.println(paths);
     }
 
@@ -36,6 +59,7 @@ public class RootToLeafPaths {
         root.right.right = new Node(7);
 
         findRootToLeafPaths(root);
+        findRootToLeafPaths2(root);
     }
 }
 //      1
