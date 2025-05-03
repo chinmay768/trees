eff81a402075de967b48b424e176b87116eb18f8
chinmay768 <chinmaysahani07@gmail.com>
2025-05-03 17:21:12 +0530

    Inorder Traversal

--- Striver/src/InorderTraversal.java
+++ Striver/src/InorderTraversal.java
@@ -1,8 +1,45 @@
+import java.util.*;
+
 public class InorderTraversal {
 
-    public static void main(String[] args) {
-        Node node = new Node(1);
+    public static void inorderRecursive(Node root) {
+        if (root == null) {
+            return;
+        }
 
-        System.out.println(node.data);
+        inorderRecursive(root.left);
+        System.out.println(root.data);
+        inorderRecursive(root.right);
+    }
+
+    public static void inorderIterative(Node root){
+        Stack<Node> st = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return;
+
+        while (root != null || !st.isEmpty()){
+            if(root != null){
+                st.push(root);
+                root = root.left;
+            }else {
+                Node topNode = st.pop();
+                list.add(topNode.data);
+                root = topNode.right;
+            }
+        }
+
+        System.out.println(list);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        inorderIterative(root);
     }
 }
