6e3041e7f677e37aa34a474e55ace7ed31f60537
chinmay768 <chinmaysahani07@gmail.com>
2025-05-17 23:46:00 +0530

    Construct Tree From IN and POST

---
+++ Striver/src/ConstructTreeFromPreorderAndInorder.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class ConstructTreeFromPreorderAndInorder {
+
+    public Node buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> inMap = new HashMap<>();
+
+        for (int i = 0; i < inorder.length; i++){
+            inMap.put(inorder[i], i);
+        }
+
+        Node root = buildTree(preorder, 0, preorder.length - 1, inorder,
+                0, inorder.length - 1, inMap);
+
+        return root;
+    }
+
+    public static Node buildTree(int[] preorder, int preStart, int preEnd,
+                                 int[] inorder, int inStart, int inEnd,
+                                 Map<Integer, Integer> inMap){
+        if(preStart > preEnd || inStart > inEnd) return null;
+
+        Node root = new Node(preorder[preStart]);
+
+        int inorderRootIdx = inMap.get(preorder[preStart]);
+        int leftElmsCount = inorderRootIdx - inStart;
+
+        root.left = buildTree(preorder, preStart + 1, preStart + leftElmsCount,
+                inorder, inStart, inorderRootIdx - 1, inMap);
+
+        root.right = buildTree(preorder, preStart + leftElmsCount + 1, preEnd,
+                inorder, inorderRootIdx + 1, inEnd, inMap);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
