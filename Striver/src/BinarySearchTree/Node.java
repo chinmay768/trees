a8a4f0b6235aaa580beb7b70cf5e62224213a330
chinmay768 <chinmaysahani07@gmail.com>
2025-05-23 23:48:29 +0530

    Search In BST

---
+++ Striver/src/BinarySearchTree/Node.java
@@ -0,0 +1,14 @@
+package BinarySearchTree;
+
+public class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
---
+++ Striver/src/BinarySearchTree/SearchInBST.java
@@ -0,0 +1,16 @@
+package BinarySearchTree;
+
+public class SearchInBST {
+
+    public static Node searchBST(Node root, int val) {
+        while (root != null && root.data != val){
+            root = root.data > val? root.left : root.right;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
