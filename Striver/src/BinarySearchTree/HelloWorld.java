75e0ff1456496ba4dc2ae9302cc939ff5a786459
chinmay768 <chinmaysahani07@gmail.com>
2025-05-26 23:04:09 +0530

    Validate BST

--- Striver/src/BinarySearchTree/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package BinarySearchTree;
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
---
+++ Striver/src/BinarySearchTree/ValidateBST.java
@@ -0,0 +1,34 @@
+package BinarySearchTree;
+
+import BinaryTree.Node;
+
+public class ValidateBST {
+
+    public static boolean isValidBST(Node root){
+        return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public static boolean isValidBST(Node root, int minRange, int maxRange) {
+        if(root == null) return true;
+
+        if(root.data < minRange || root.data > maxRange) return false;
+
+        return isValidBST(root.left, minRange, root.data) &&
+                isValidBST(root.right, root.data, maxRange);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(5);
+        root.right = new Node(13);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+        root.left.right.right = new Node(9);
+        root.left.left.left = new Node(2);
+        root.left.left.right = new Node(4);
+        root.right.left = new Node(11);
+        root.right.right = new Node(14);
+
+        System.out.println(isValidBST(root));
+    }
+}
